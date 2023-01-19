5bbeb4ef5917d2078d29081f41d40281be6327cb
Laura McGowan <prime.lmcg@gmail.com>
2023-01-19 15:20:57 -0600

    index.js store & reducers written

--- src/index.js
+++ src/index.js
@@ -3,4 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//import redux
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+//reducers
+const pizzaMenu = (state = [], action) => {
+    return state;
+}
+
+
+const pizzaOrder = (state = [], action) => {
+    return state;
+}
+
+
+const customerInfo = (state = [], action) => {
+    return state;
+}
+
+
+const customerOrder = (state = [], action) => {
+    return state;
+}
+
+const orderCost = (state = [], action) => {
+    return state;
+}
+   
+
+//store
+const reduxStore = createStore(combineReducers ({
+    //reducers go here
+}))
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={reduxStore}>
+            <App />, 
+        </Provider>
+    </React.StrictMode>
+,document.getElementById('root'));
