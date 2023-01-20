fb5b1ab217bc55fdff51eb3d16e934170c9d8c79
Adam Bergstrom <abx1138@gmail.com>
2023-01-20 09:15:56 -0600

    completed PizzaMenu and PizzaItem Pizzas are rendering to the DOM - Needs better styling

--- src/components/PizzaMenu/PizzaMenu.jsx
+++ src/components/PizzaMenu/PizzaMenu.jsx
@@ -1,20 +1,26 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { useEffect } from 'react'
 import axios from 'axios'
+import PizzaItem from './PizzaItem';
 
 
 function PizzaMenu ( {fetchPizzaMenu} ){
 
-const pizzaList = useSelector(store => store.pizzaList);
+const pizzaMenu = useSelector(store => store.pizzaMenu);
 
 useEffect(() => {
-    fetchPizzaMenu();
+    fetchPizzaMenu()
   }, []) // 👈 don't forget the empty array!
 
     return(
     <section>
       <h2>All pizzas</h2>
+      <ul>
+        {pizzaMenu.map((pizza) => {
+          return <PizzaItem key={pizza.id} pizza={pizza} fetchPizzaMenu={fetchPizzaMenu} />
+        })}
+      </ul>
     </section>
-    )
+    );
 }
-export default PizzaMenu
+export default PizzaMenu;
